b147a0c13c64eebd9cfe8f18515754b68883c343
muhammadega21 <dermawane988@gmail.com>
2024-11-02 11:12:12 +0700

    Mengupload folder pertemuan 2 dan 3

---
+++ pertemuan2/index.js
@@ -0,0 +1,18 @@
+let mahasiswa = "Ega";
+const kelas = "sistem informasi";
+
+console.log(mahasiswa)
+
+
+
+// 1.git init aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// 2.git add .
+// 3.git commit -m "comment"
+
+/* asdadsad
+asdasd
+
+
+
+
+*/
---
+++ pertemuan3/index.js
@@ -0,0 +1,10 @@
+var kegiatan = "Latihan";
+
+// Silahkan Perkenalkan diri kalian
+// buat variabel berisi data diri berupa nama,jurusab,alamat,kelas yang dimasuki, dan hobi
+// variasikan jenis variable yang telah dipelajari tadi (var,let,const)
+
+var nama = "ega"
+let jurusan_asdasd_asd = "sistem informasi"
+
+console.log(kegiatan)
